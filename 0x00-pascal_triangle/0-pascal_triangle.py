2cd1dcfcc60f3aa7ea170d3c40567cc62af581f5
Abdulaziz Ahmed <azizazu0934@gmail.com>
2023-03-22 17:01:20 +0000

    Pascal Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
-"""Interview challenge Pascal Triangle"""
+""" Interview Challenge Pascal Triangle"""
 
 
 def pascal_triangle(n):
-    """returns a list of lists of numbers 
+    """returns a list of lists of numbers
     representing the pascal triangle"""
     if n <= 0:
         return []
 
-    triangle = [[1]]
+    pascal_triangle = [0] * n
 
-    for i in range(1, n):
-        """Loop over each row of the triangle, 
-        adding the next row based on the previous row"""
-        previous_row = triangle[i - 1]
-        current_row = [1]
+    for i in range(n):
+        # define a row and fill first and last idx with 1
+        new_row = [0] * (i+1)
+        new_row[0] = 1
+        new_row[len(new_row) - 1] = 1
 
         for j in range(1, i):
-            current_row.append(previous_row[j - 1] + previous_row[j])
-        current_row.append(1)
-        triangle.append(current_row)
+            if j > 0 and j < len(new_row):
+                a = pascal_triangle[i - 1][j]
+                b = pascal_triangle[i - 1][j - 1]
+                new_row[j] = a + b
 
-    return triangle
+        pascal_triangle[i] = new_row
+
+    return pascal_triangle
