b429b056595a9ee5ab9c0700ecbf0b2065baf38a
Abdulaziz Ahmed <azizazu0934@gmail.com>
2023-03-22 11:11:11 +0000

    Pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,27 @@
+def pascal_triangle(n):
+    """
+    Return an empty list if n <= 0
+    """
+    if n <= 0:
+        return []
+
+    """ 
+    Create a list to hold the triangle
+    """
+    triangle = [[1]]
+
+    """ 
+    Loop over each row of the triangle, adding the next row based on the previous row
+    """
+    for i in range(1, n):
+        previous_row = triangle[i - 1]
+        current_row = [1]
+        for j in range(1, i):
+            current_row.append(previous_row[j - 1] + previous_row[j])
+        current_row.append(1)
+        triangle.append(current_row)
+
+    """ 
+    Return the completed triangle
+    """
+    return triangle
