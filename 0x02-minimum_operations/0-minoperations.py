307c3baeaa44e1a096da4ae3b939e54a607e2aa9
Abdulaziz Ahmed <azizazu0934@gmail.com>
2023-04-06 21:51:20 +0000

    0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -9,20 +9,13 @@ def minOperations(n):
     """
     Returns the fewest number of operations needed to result in exactly
     n H characters in the file.
-    Returns an integer
-    If n is impossible to achieve, return 0
     """
-    if n == 1:
-        return 0
-    operations = 0
-    current = 1
-    clipboard = 0    
-    while current < n:
-        if (n - current) % current == 0 and clipboard == current // 2:
-            clipboard = current
-            current += clipboard
-            operations += 2
-        else:
-            current += clipboard
-            operations += 1
-    return operations
+    ops = 0
+    min_ops = 2
+    while n > 1:
+        while n % min_ops == 0:
+            ops += min_ops
+            n /= min_ops
+        min_ops += 1
+    return ops
+
