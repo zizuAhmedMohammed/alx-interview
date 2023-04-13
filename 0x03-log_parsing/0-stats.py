e698c0d90a27948c4d402a2dfade73dfe7ec5c5c
Abdulaziz Ahmed <azizazu0934@gmail.com>
2023-04-13 20:06:50 +0000

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,37 +1,36 @@
 #!/usr/bin/python3
 """This script reads stdin line by line and computes metrics."""
 
-
 import sys
 
-cache_dict = {'200': 0, '301': 0, '400': 0, '401': 0,
-              '403': 0, '404': 0, '405': 0, '500': 0}
-total_file_size = 0
-count = 0
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
+total_size = 0
+counter = 0
 
 try:
     for line in sys.stdin:
         line_list = line.split(" ")
         if len(line_list) > 4:
-            status_code = line_list[-2]
-            file_size = int(line_list[-1])
-            if status_code in cache_dict.keys():
-                cache_dict[status_code] += 1
-            total_file_size += file_size
-            count += 1
-        
-        if count == 10:
-            count = 0
-            print('Total File size: {}'.format(total_file_size))
-            for key, value in sorted(cache_dict.items()):
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
+            counter += 1
+
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
                 if value != 0:
                     print('{}: {}'.format(key, value))
 
-except Exception as e:
+except Exception as err:
     pass
 
 finally:
-    print('Total File size: {}'.format(total_file_size))
-    for key, value in sorted(cache_dict.items()):
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
         if value != 0:
             print('{}: {}'.format(key, value))
