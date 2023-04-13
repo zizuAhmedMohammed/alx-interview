9f488f3e4a797a46d416725433a850fe70ca6c11
Abdulaziz Ahmed <azizazu0934@gmail.com>
2023-04-13 19:53:45 +0000

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,64 +1,37 @@
 #!/usr/bin/python3
+"""This script reads stdin line by line and computes metrics."""
+
 
 import sys
-import signal
 
-"""Initialize variables"""
-total_size = 0
-status_code_dict = {}
+cache_dict = {'200': 0, '301': 0, '400': 0, '401': 0,
+              '403': 0, '404': 0, '405': 0, '500': 0}
+total_file_size = 0
+count = 0
 
-def print_stats():
-    """Define function to print statistics"""
-    global total_size
-    global status_code_dict
-    
-    """Print total file size"""
-    print(f'Total file size: {total_size}')
-    
-    """Sort and print number of lines by status code"""
-    sorted_status_codes = sorted(status_code_dict.keys())
-    for status_code in sorted_status_codes:
-        if status_code in status_code_dict:
-            print(f'{status_code}: {status_code_dict[status_code]}')
+try:
+    for line in sys.stdin:
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            status_code = line_list[-2]
+            file_size = int(line_list[-1])
+            if status_code in cache_dict.keys():
+                cache_dict[status_code] += 1
+            total_file_size += file_size
+            count += 1
+        
+        if count == 10:
+            count = 0
+            print('Total File size: {}'.format(total_file_size))
+            for key, value in sorted(cache_dict.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
 
+except Exception as e:
+    pass
 
-def process_line(line):
-    """Define function to process a line of input"""
-    global total_size
-    global status_code_dict
-    
-    line = line.strip()
-    
-    try:
-        _, _, request, status_code_str, file_size_str, _ = line.split(' ')
-        if 'GET /projects/260 HTTP/1.1' in request:
-            """If the request contains the specified string, update metrics"""
-            status_code = int(status_code_str)
-            file_size = int(file_size_str)
-            total_size += file_size
-            
-            if status_code not in status_code_dict:
-                status_code_dict[status_code] = 1
-            else:
-                status_code_dict[status_code] += 1
-    except ValueError:
-        """If the line doesn't match the input format, skip it"""
-        pass
-
-def signal_handler(signal, frame):
-    """Define a signal handler to print statistics when interrupted"""
-    print_stats()
-    sys.exit(0)
-
-
-signal.signal(signal.SIGINT, signal_handler)
-
-line_num = 0
-
-for line in sys.stdin:
-    process_line(line)
-    
-    line_num += 1
-    if line_num % 10 == 0:
-        """Print statistics every 10 lines"""
-        print_stats()
+finally:
+    print('Total File size: {}'.format(total_file_size))
+    for key, value in sorted(cache_dict.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
