afa30ba260d74eb465fdfebd80e16a633a17fa4f
Abdulaziz Ahmed <azizazu0934@gmail.com>
2023-04-20 10:08:55 +0000

    0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""
+This module contains a function for checking if a list of integers
+represents a valid UTF-8 encoding.
+"""
+
+def valid_utf8(data):
+    """
+    Determines if a given data set represents a valid UTF-8 encoding.
+    returns True if valid else False
+    """
+    count = 0
+    for byte in data:
+        if count == 0:
+            if (byte >> 5) == 0b110:
+                count = 1
+            elif (byte >> 4) == 0b1110:
+                count = 2
+            elif (byte >> 3) == 0b11110:
+                count = 3
+            elif (byte >> 7):
+                return False
+        else:
+            if (byte >> 6) != 0b10:
+                return False
+            count -= 1
+    return count == 0
