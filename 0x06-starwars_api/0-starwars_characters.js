89669961dd3fec576b36ef081029924d5ca581b7
Abdulaziz Ahmed <azizazu0934@gmail.com>
2023-05-04 09:19:44 +0000

    starwars characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,16 +3,12 @@ const args = process.argv;
 const filmUrl = 'https://swapi-api.alx-tools.com/api/films/';
 const movieUrl = `${filmUrl}${args[2]}/`;
 
-// Require 'request' module for making HTTP requests
 const request = require('request');
 
-// Function to make requests to the character URLs
 function charRequest (idx, url, characters, limit) {
   if (idx === limit) {
     return;
   }
-  
-  // Make HTTP request to the character URL
   request(url, function (error, response, body) {
     if (!error) {
       const rbody = JSON.parse(body);
@@ -25,7 +21,6 @@ function charRequest (idx, url, characters, limit) {
   });
 }
 
-// Main function
 request(movieUrl, function (error, response, body) {
   if (error == null) {
     const fbody = JSON.parse(body);
